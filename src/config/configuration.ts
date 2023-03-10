d52f1725db65dcf22f30516fa7ec40997228597d
kurokeita <kuro.keita94@gmail.com>
2023-03-10 22:02:46 +0700

    update readme and jwt implementation

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,3 +1,8 @@
+import * as fs from 'fs';
+
+const privateKey = fs.readFileSync('storage/oauth/private.pem', 'utf8');
+const publicKey = fs.readFileSync('storage/oauth/public.crt', 'utf8');
+
 export default () => ({
   app: {
     port: parseInt(process.env.APP_PORT ?? '3000', 10),
@@ -11,8 +16,8 @@ export default () => ({
     database: process.env.DB_DATABASE,
   },
   jwt: {
-    access_token_private_key: process.env.JWT_PRIVATE_KEY,
-    access_token_public_key: process.env.JWT_PUBLIC_KEY,
+    access_token_private_key: privateKey,
+    access_token_public_key: publicKey,
     access_token_expiration_time: process.env.JWT_EXPIRATION_TIME,
   },
 });
