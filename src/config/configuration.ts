9e78dbb5dca9da2a6d23c2b58095645cc19f46b8
Kuro Keita <kuro.keita94@gmail.com>
2023-03-24 10:17:48 +0000

    set env when init workspace

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -8,13 +8,6 @@ export default () => ({
     port: parseInt(process.env.APP_PORT ?? '3000', 10),
     domain: process.env.APP_DOMAIN ?? 'localhost',
   },
-  db: {
-    host: process.env.DB_HOST ?? 'localhost',
-    port: parseInt(process.env.DB_PORT ?? '3306', 10),
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-  },
   jwt: {
     access_token_private_key: privateKey,
     access_token_public_key: publicKey,
